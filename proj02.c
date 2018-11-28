e6b6d784aabfca25ed7129142a773c6c28f6d555
Paulo Victor <pv.paulovictor.vp@gmail.com>
2018-11-28 20:27:40 -0200

    Consertando bug no final da leitura

--- proj02.c
+++ proj02.c
@@ -2,33 +2,31 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    unsigned int number = 0, shift = 0, page = 0;
-    int i = 0; //Retirar o i para rodar todo o arquivo
-    if (argc == 1) {
-      printf("meu irmao, rode o programa assim:\n");
-      printf("gcc proj02.c && ./a.out addresses.txt\n\n");
-      return 0;
-    }
-    FILE *fp = fopen(argv[1], "r");
+    unsigned int number = 0; /* The number we will take of the file */
+    unsigned int page   = 0; /* The number of the page, bits 8-15 */
+    unsigned int shift  = 0; /* The size os the shift on the page, bits 0-7 */
+    int oldNumber = -1;      /* Used to compare and stop the loop */
+    
+    FILE *fp = fopen(argv[1], "r"); /* Open the argv passed */
     if (fp == NULL) {
-      printf("falha ao ler o arquivo \"%s\"\n",argv[1]);
-      return 0;
-    }
-    while(1) {
-        //printf("scan = %d\n", fscanf(fp, "%u", &number));
-        if (fscanf(fp, "%u", &number) != 1) {
-          break;
-        }
-        page = number << 16;
-        page = page >> (16 + 8);
-        shift = number << (16 + 8);
-        shift = shift >> (16 + 8);
-        //printf("page=%d\n", number);
+        /* See if the argv passed if valid */
+        printf("Falha ao ler o arquivo '%s'\n", argv[1]); /* Print a mensage 
+                                                             of error */
+    } else {
+        while(fscanf(fp, "%u", &number) && number != oldNumber) { /* Compare 
+                        if is reading the argv and stop if the value repeat */
+            page = number << 16; /* Take off the fisrt 16 bits */
+            page = page >> (16 + 8); /* Take off the last 8 and normalize */
+            shift = number << (16 + 8); /* Take off the fisrt 16 and 8 bits */
+            shift = shift >> (16 + 8); /* Normalize */
+            //printf("Page: %u\t", page); /* Print to debug */
+            //printf("Shift: %u\n", shift); /* Print to debug */
 
-        //i++;
-        //if(i > 5) break;
-        //printf("page=%d, shift=%d\n",page,shift); // para debug
+            oldNumber = number; /* Att the oldNumber to compare in the next 
+                                   loop */
+        }
+        fclose(fp); /* Close the file */
     }
-    fclose(fp);
+
     return 0;
 }
