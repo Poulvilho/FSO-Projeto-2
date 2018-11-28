075b875af8a116300b942fc81b2f2795b79e3230
marcelo046 <marc3lo046@gmail.com>
2018-11-28 15:42:00 -0200

    bug de leitura dos endereços consertado

--- proj02.c
+++ proj02.c
@@ -9,16 +9,26 @@ int main(int argc, char *argv[]) {
       printf("gcc proj02.c && ./a.out addresses.txt\n\n");
       return 0;
     }
-    //FILE *fp = fopen(argv[1], "r"); // Retirar o coment�rio para ler o arquivo
-    while(/*fscanf(fp, "%u", &number)*/i<3) { //Retirar o i para rodar todo o arquivo
-        //fscanf(fp, "%u", &number);
+    FILE *fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+      printf("falha ao ler o arquivo \"%s\"\n",argv[1]);
+      return 0;
+    }
+    while(1) {
+        //printf("scan = %d\n", fscanf(fp, "%u", &number));
+        if (fscanf(fp, "%u", &number) != 1) {
+          break;
+        }
         page = number << 16;
         page = page >> (16 + 8);
         shift = number << (16 + 8);
         shift = shift >> (16 + 8);
-        i++; // Retirar o i para rodar todo o arquivo
+        //printf("page=%d\n", number);
+
+        //i++;
+        //if(i > 5) break;
         //printf("page=%d, shift=%d\n",page,shift); // para debug
     }
-    //fclose(fp);
+    fclose(fp);
     return 0;
 }
